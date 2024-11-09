18ff9f26e26d18204cc87e21e0fc5e4b9d6185e8
Jacob Jablon (RIT Student) <jrj8250@rit.edu>
2024-11-09 16:00:54 -0500

    initial commit

---
+++ theLongGame/theLongGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace theLongGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
